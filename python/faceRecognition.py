c370e0fc2cec8c74b23acd1ea65a2d9048832448
matheuscampbell <matheuscampbell@users.noreply.github.com>
2022-10-28 01:30:30 -0300

    atualização

--- python/faceRecognition.py
+++ python/faceRecognition.py
@@ -3,6 +3,9 @@ import sys
 picture_of_me = face_recognition.load_image_file(sys.argv[1])
 my_face_encoding = face_recognition.face_encodings(picture_of_me)[0]
 
-results = face_recognition.compare_faces([my_face_encoding], my_face_encoding)
+picture_of_me2 = face_recognition.load_image_file(sys.argv[2])
+my_face_encoding2 = face_recognition.face_encodings(picture_of_me2)[0]
+
+results = face_recognition.compare_faces([my_face_encoding], my_face_encoding2)
 
 print(results)
